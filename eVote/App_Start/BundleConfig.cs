8de0f6677129303b15d060feb4350579516521bd
Pesantez <SPesantez@oas.org>
2018-04-10 18:39:09 -0400

    Init Commit

--- eVote/App_Start/BundleConfig.cs
+++ eVote/App_Start/BundleConfig.cs
@@ -20,12 +20,15 @@ namespace eVote
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/mdb.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/font-awesome.min.css",
+                "~/Content/bootstrap.min.css",
+                "~/Content/mdb.min.css", 
+                "~/Content/site.css"));
         }
     }
 }
