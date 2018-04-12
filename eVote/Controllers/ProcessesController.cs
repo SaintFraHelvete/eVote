85c081be134b4e689fd9b21f9930cb63da283c63
SaintFraHelvete <saints16@gmail.com>
2018-04-12 18:15:01 -0400

    Imgs added and navbar edited

---
+++ eVote/Controllers/ProcessesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace org.oas.cidh.eVote.Controllers
+{
+    public class ProcessesController : Controller
+    {
+        // GET: Processes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pending()
+        {
+            return View();
+        }
+
+        public ActionResult Closed()
+        {
+            return View();
+        }
+
+        public ActionResult InProgress()
+        {
+            return View();
+        }
+    }
+}
