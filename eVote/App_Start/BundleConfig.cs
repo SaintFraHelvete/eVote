8865d0383cff98703c566a46b9ba9ea042541146
SaintFraHelvete <saints16@gmail.com>
2018-05-01 18:34:12 -0400

    File structure changed. Flipping cards in development.

--- eVote/App_Start/BundleConfig.cs
+++ eVote/App_Start/BundleConfig.cs
@@ -9,26 +9,26 @@ namespace eVote
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/mdb.min.js",
-                      "~/Scripts/respond.js"));
+                "~/Content/scripts/bootstrap.js",
+                "~/Content/scripts/mdb.min.js",
+                "~/Content/scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/font-awesome.min.css",
-                "~/Content/bootstrap.min.css",
-                "~/Content/mdb.css", 
-                "~/Content/site.css"));
+                "~/Content/styles/font-awesome.min.css",
+                "~/Content/styles/bootstrap.min.css",
+                "~/Content/styles/mdb.css",
+                "~/Content/styles/site.css"));
         }
     }
 }
